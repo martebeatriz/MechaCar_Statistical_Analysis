b2d21e23737f965a34c2f7972e718b6a269ae299
Beatriz <owner1@MacBook-Air.local>
2022-09-26 01:07:10 -0400

    added tables for suspension coil stat summary and a table for summaries by lot

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -9,3 +9,16 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data= MechaCar_mpg_df ))
+
+#Import Suspension_Coil.csv file 
+Suspension_Coil_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create a total_summary dataframe using the summarize() function 
+#to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary <-  Suspension_Coil_df  %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance= var(PSI), SD= sd(PSI) )
+
+#Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions 
+#to group each manufacturing lot by the mean, median, variance, 
+#and standard deviation of the suspension coil’s PSI column.
+lot_summary <- Suspension_Coil_df  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance= var(PSI), SD= sd(PSI) )
+
